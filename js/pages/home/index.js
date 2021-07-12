048f1e99f3effd8d92da632dbc9b152bb011c6c8
Igor M <russo.programmisto@gmail.com>
2021-07-13 01:10:11 +0300

    ...

--- js/pages/home/index.js
+++ js/pages/home/index.js
@@ -1,3 +1,5 @@
+const useLegacyApiForZipRequest = true;
+
 $(document).ready(() => {
     
     const zipForms = Object.freeze({
@@ -72,75 +74,7 @@ $(document).ready(() => {
                     )
                 );
             },
-            true
+            useLegacyApiForZipRequest
         );
     });
-
-    /*
-     * Top `check coverage` button.
-     */
-    /*const checkCoverageTopButton = document.getElementById("zipcode-check-1");
-    
-    if (checkCoverageTopButton instanceof HTMLInputElement) {
-        $(checkCoverageTopButton).on("click", function (e) {
-            e.preventDefault()
-            let zip_code = $("#zipcode-2").val();
-            $(this).val("Please wait...")
-            
-            if (zip_code.length) {
-                checkZip(zip_code, (response, isValid) => {
-                    if (isValid) {
-                        $(".zip-check").html("Great news! We do have coverage in your area. Click below to choose your landline service plan, or give us a call at (855) 615-0667.")
-                        $(".zipcode-container").css("display", "none")
-                        $(".zip-check").append('<br><a href="/checkout-landline/choose-a-plan"><button class="primary-button" style="top: auto; right: auto;">Start your landline service</button></a>')
-                    } else {
-                        $(".zip-check").html(response)
-                        $(".start-landline-btn").attr("data-wait", "Start my landline service")
-                        $(".start-landline-btn").val("Start my landline service")
-                    }
-    
-                    $(".coverage_response").css("font-size", "12px")
-                    $(".coverage_response").css("line-height", "0")
-                    $(".coverage_response").css("margin-top", "0")
-                });
-            } else {
-                $(".start-landline-btn").val("Start my landline service")
-                $(".zip-check").html('<p style="color: #ff0000">Please type your zip code<p>')
-            }
-        });
-    }*/
-
-    /*
-     * Bottom `check coverage` button.
-     */
-    /*const checkCoverageBottomButton = document.getElementById("zipcode-check-2");
-    
-    if (checkCoverageBottomButton instanceof HTMLInputElement) {
-        $("#zipcode-check-2").on("click", function (e) {
-            e.preventDefault()
-            let zip_code = $("#zipcode-bottom").val();
-            $(this).val("Please wait...")
-    
-            if (zip_code.length) {
-                checkZip(zip_code, (response, isValid) => {
-                    if (isValid) {
-                        $("#zip-check-bottom").html("Great news! We do have coverage in your area. Click below to choose your landline service plan, or give us a call at (855) 615-0667.")
-                        $(".zipcode-container").css("display", "none")
-                        $("#zip-check-bottom").append('<br><a href="/checkout-landline/choose-a-plan"><button class="primary-button" style="top: auto; right: auto;">Start your landline service</button></a>')
-                    } else {
-                        $("#zip-check-bottom").html(response)
-                        $(".start-landline-btn").attr("data-wait", "Start my landline service")
-                        $(".start-landline-btn").val("Start my landline service")
-                    }
-    
-                    $(".coverage_response").css("font-size", "12px")
-                    $(".coverage_response").css("line-height", "0")
-                    $(".coverage_response").css("margin-top", "0")
-                });
-            } else {
-                $(".start-landline-btn").val("Start my landline service")
-                $("#zip-check-bottom").html('<p style="color: #ff0000">Please type your zip code<p>')
-            }
-        });
-    }*/
 });
