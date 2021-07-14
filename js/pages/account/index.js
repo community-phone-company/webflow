7ae4a012727030fb0a10cacf495b376391b4ec32
Igor M <russo.programmisto@gmail.com>
2021-07-14 14:54:31 +0300

    ...

--- js/pages/account/index.js
+++ js/pages/account/index.js
@@ -15,10 +15,43 @@ $(document).ready(() => {
             const isEmail = emailRegularExpression.test(email);
 
             if (isEmail) {
-                HotjarIntegration.send({
+                /*HotjarIntegration.send({
                     "Email": email
-                });
+                });*/
             }
         };
     }
+
+    /**
+     * Here we handle submit button's click.
+     */
+    const submitButton = document.querySelectorAll("input.continue_account")[0];
+
+    $(submitButton).on("click", (event) => {
+        const firstName = $("#First-name").val();
+        Store.local.write(CheckoutFlowStoreKey.firstName, firstName);
+
+        const lastName = $("#Last-name").val();
+        Store.local.write(CheckoutFlowStoreKey.lastName, lastName);
+
+        const phone = $("#Contact-number").val();
+        Store.local.write(CheckoutFlowStoreKey.phone, phone);
+
+        const email = $("#Email").val();
+        Store.local.write(CheckoutFlowStoreKey.email, email);
+
+        HotjarIntegration.send({
+            "Email": email
+        });
+
+        ActiveCampaignIntegration.createOrUpdateContact(
+            new ActiveCampaignContact(
+                email,
+                firstName,
+                lastName,
+                phone,
+                []
+            )
+        );
+    });
 });
