6e2d205e69d772e207b045dc2477bd34d2fe2651
Igor M <russo.programmisto@gmail.com>
2021-07-19 23:02:59 +0300

    ...

---
+++ js/pages/business-flow/choose-business-number/phone-number-form.js
@@ -0,0 +1,190 @@
+class PhoneNumberForm {
+
+    /**
+     * @constructor
+     * @param {string} formSelector 
+     */
+    constructor(formSelector) {
+        const form = document.querySelectorAll(formSelector)[0]
+
+        if (form instanceof HTMLFormElement) {
+            this._form = form;
+        } else {
+            throw new Error(`${formSelector} is not HTMLFormElement!`);
+        }
+    }
+
+    getForm = () => {
+        return this._form;
+    }
+
+    /**
+     * City input.
+     * @returns {HTMLInputElement} `HTMLInputElement` instance.
+     */
+    getCityInput = () => {
+        /**
+         * Some kind of lazy load implementation.
+         * Once we call this method, it will remember the element
+         * so we won't need to search it again over the entire document.
+         */
+        if (this._cityInput) {
+            return this._cityInput;
+        }
+
+        const selector = "#city";
+        const input = this.getForm().querySelectorAll(selector)[0];
+        
+        if (input instanceof HTMLInputElement) {
+            this._cityInput = input;
+            return input;
+        } else {
+            throw new Error(`${selector} is not an HTMLInputElement.`);
+        }
+    }
+
+    /**
+     * Area code input.
+     * @returns {HTMLInputElement} `HTMLInputElement` instance.
+     */
+     getAreaCodeInput = () => {
+        /**
+         * Some kind of lazy load implementation.
+         * Once we call this method, it will remember the element
+         * so we won't need to search it again over the entire document.
+         */
+        if (this._areaCodeInput) {
+            return this._areaCodeInput;
+        }
+
+        const selector = "#Area-code";
+        const input = this.getForm().querySelectorAll(selector)[0];
+        
+        if (input instanceof HTMLInputElement) {
+            this._areaCodeInput = input;
+            return input;
+        } else {
+            throw new Error(`${selector} is not an HTMLInputElement.`);
+        }
+    }
+
+    /**
+     * Digits input.
+     * @returns {HTMLInputElement} `HTMLInputElement` instance.
+     */
+     getDigitsInput = () => {
+        /**
+         * Some kind of lazy load implementation.
+         * Once we call this method, it will remember the element
+         * so we won't need to search it again over the entire document.
+         */
+        if (this._digitsInput) {
+            return this._digitsInput;
+        }
+
+        const selector = "#digits";
+        const input = this.getForm().querySelectorAll(selector)[0];
+        
+        if (input instanceof HTMLInputElement) {
+            this._digitsInput = input;
+            return input;
+        } else {
+            throw new Error(`${selector} is not an HTMLInputElement.`);
+        }
+    }
+
+    /**
+     * Toll free switcher.
+     * @returns {HTMLDivElement} `HTMLDivElement` instance.
+     */
+     getDigitsInput = () => {
+        /**
+         * Some kind of lazy load implementation.
+         * Once we call this method, it will remember the element
+         * so we won't need to search it again over the entire document.
+         */
+        if (this._tollFreeSwitcher) {
+            return this._tollFreeSwitcher;
+        }
+
+        const selector = ".switcher";
+        const switcher = this.getForm().querySelectorAll(selector)[0];
+        
+        if (input instanceof HTMLDivElement) {
+            this._tollFreeSwitcher = switcher;
+            return switcher;
+        } else {
+            throw new Error(`${selector} is not an HTMLDivElement.`);
+        }
+    }
+
+    /**
+     * Column.
+     * @param {number} index Column index.
+     * @returns {PhoneNumberFormColumn} `PhoneNumberFormColumn` instance.
+     */
+    getColumnWithIndex = (index) => {
+        if (!this._columns) {
+            const selector = "div.columns-6.w-row div.w-col.w-col-6";
+            this._columns = Array.from(
+                this.getForm().querySelectorAll(selector)
+            ).map(columnDiv => {
+                return new PhoneNumberFormColumn(columnDiv);
+            });
+        }
+
+        return this._columns[index];
+    }
+
+    /**
+     * Left column.
+     * @returns {PhoneNumberFormColumn} `PhoneNumberFormColumn` instance.
+     */
+    getLeftColumn = () => {
+        return this.getColumnWithIndex(0);
+    }
+
+    /**
+     * Right column.
+     * @returns {PhoneNumberFormColumn} `PhoneNumberFormColumn` instance.
+     */
+    getRightColumn = () => {
+        return this.getColumnWithIndex(1);
+    }
+
+    /**
+     * Submit button.
+     * @returns {HTMLInputElement} `HTMLInputElement` instance.
+     */
+     getSubmitButton = () => {
+        /**
+         * Some kind of lazy load implementation.
+         * Once we call this method, it will remember the element
+         * so we won't need to search it again over the entire document.
+         */
+        if (this._submitButton) {
+            return this._submitButton;
+        }
+
+        const selector = "input[type='submit']";
+        const input = this.getForm().querySelectorAll(selector)[0];
+        
+        if (input instanceof HTMLInputElement) {
+            this._submitButton = input;
+            return input;
+        } else {
+            throw new Error(`${selector} is not an HTMLInputElement.`);
+        }
+    }
+}
+
+class PhoneNumberFormColumn {
+
+    /**
+     * @constructor
+     * @param {HTMLDivElement} columnDiv 
+     */
+    constructor(columnDiv) {
+        this.columnDiv = columnDiv;
+    }
+}
