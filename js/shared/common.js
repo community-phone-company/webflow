5bac7207af862bf9d4fdd246dfa569ffa0f6b13c
Igor M <russo.programmisto@gmail.com>
2021-08-02 20:08:51 +0300

    ..........

--- js/shared/common.js
+++ js/shared/common.js
@@ -12,6 +12,29 @@ logger.setBlockedDomains([
 /**
  * Send user ID and other information to Hotjar.
  */
-HotjarIntegration.send({
-    "Last Visit": new Date(Date.now()).toISOString()
-});
+(() => {
+    HotjarIntegration.send({
+        "Last Visit": new Date(Date.now()).toISOString()
+    });
+    
+    const emails = {
+        businessFlow: Store.local.read(
+            Store.keys.businessFlow.email
+        ),
+        onboardingFlow: Store.local.read(
+            Store.keys.onboardingFlow.email
+        ),
+        checkoutFlow: Store.local.read(
+            Store.keys.checkoutFlow.email
+        )
+    };
+    const emailToSend = emails.businessFlow
+        ?? emails.onboardingFlow
+        ?? emails.checkoutFlow;
+    
+    if (emailToSend) {
+        HotjarIntegration.send({
+            "Email": emailToSend
+        });
+    }
+})();
