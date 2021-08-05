6bef39d3415a0247388748633d82413f898de6ac
Igor M <russo.programmisto@gmail.com>
2021-08-06 01:06:30 +0300

    ...

--- js/shared/elements_legacy.js
+++ js/shared/elements_legacy.js
@@ -1,6 +1,12 @@
 
 $(document).ready(function () {
 
+    // TODO: REMOVE WHEN FINISHED WITH TESTING!
+    if (!IS_PRODUCTION) {
+        console.log("webflow-legacy.js blocked");
+        return;
+    }
+
     let handset_is_selected = false
     let insuarance_is_selected = false
    let cp_order_summary = {"handset_is_selected": false, "insuarance_is_selected": false, "phone_service": "New Number", "plan_type": "Monthly"}
