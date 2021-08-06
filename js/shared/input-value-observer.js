55563f32203d24a03cb8152988515aed805b833f
Igor M <russo.programmisto@gmail.com>
2021-08-06 03:24:40 +0300

    ....

--- js/shared/input-value-observer.js
+++ js/shared/input-value-observer.js
@@ -3,37 +3,55 @@ class InputValueObserver {
     /**
      * @constructor
      * @param {HTMLInputElement} input 
+     * @param {boolean} highPerformance
      */
-    constructor(input) {
+    constructor(input, highPerformance) {
         this.input = input;
+        this.highPerformance = highPerformance;
         this._lastValue = input.value;
     }
 
     /**
-     * @param {(newValue: string) => void} onValueChanged 
+     * @param {(newValue: string) => void} onValueChanged Function that is called when input's value has changed.
      */
     startObserving = (onValueChanged) => {
         this._onValueChanged = onValueChanged;
-        this._timerId = setInterval(() => {
+
+        const checkValue = () => {
             const currentValue = this.input.value;
 
             if (this._lastValue !== currentValue) {
                 this._lastValue = currentValue;
-                
+
                 if (this._onValueChanged instanceof Function) {
                     this._onValueChanged(currentValue);
                 }
             }
-        }, InputValueObserverConfiguration.timerInterval);
+        };
+        
+        if (this.highPerformance) {
+            this.input.oninput = () => {
+                checkValue();
+            };
+        } else {
+            this._timerId = setInterval(() => {
+                checkValue();
+            }, InputValueObserverConfiguration.timerInterval);
+        }
     }
 
     stopObserving = () => {
         this._onValueChanged = undefined;
-        if (this._timerId) {
-            clearInterval(
-                this._timerId
-            );
-            this._timerId = undefined;
+        
+        if (this.highPerformance) {
+            this.input.oninput = undefined;
+        } else {
+            if (this._timerId) {
+                clearInterval(
+                    this._timerId
+                );
+                this._timerId = undefined;
+            }
         }
     }
 }
