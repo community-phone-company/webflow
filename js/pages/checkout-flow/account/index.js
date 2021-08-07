7ae40fb110e3d31b6aeef2e34891d02220ca649b
Igor M <russo.programmisto@gmail.com>
2021-08-07 03:07:54 +0300

    ....

--- js/pages/checkout-flow/account/index.js
+++ js/pages/checkout-flow/account/index.js
@@ -78,6 +78,7 @@ $(document).ready(() => {
     form.elements.howDidYouHearAboutUs.oninput = () => {
         const visibleValue = form.elements.howDidYouHearAboutUs.value;
         const value = visibleValue === howDidYouHearAboutUsPlaceholder ? "" : visibleValue;
+        logger.print(`new value: ${value}`);
         form.data.howDidYouHearAboutUs = value;
     };
     form.elements.howDidYouHearAboutUs.value = (() => {
