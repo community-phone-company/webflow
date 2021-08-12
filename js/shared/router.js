69bc77b1bad9116a3643e22d8840e57d10b375c6
Igor M <russo.programmisto@gmail.com>
2021-08-12 18:28:37 +0300

    ....

--- js/shared/router.js
+++ js/shared/router.js
@@ -1,23 +1,38 @@
 class Router {
 
-    static _default = undefined
-
-    static getDefault = () => {
-        if (!this._default) {
-            this._default = new Router();
-        }
-
-        return this._default;
-    }
-
     /**
      * @constructor
      */
     constructor() {
     }
 
+    /**
+     * Redirects to the specified page.
+     * @param {string} path Path for the page. It's recommended to use {@link RouterPath} values here.
+     */
     open = (path) => {
         window.location.href = `${window.location.host}${path}`;
+    }
+
+    /**
+     * @returns {any}
+     */
+    getParameters = () => {
+        return Object.fromEntries(
+            new URLSearchParams(
+                window.location.search
+            ).entries()
+        );
+    }
+
+    /**
+     * Reads URL parameter's value.
+     * @param {string} key URL parameter's key.
+     * @returns {any | undefined} Parameter's value or undefined.
+     */
+    getParameterValue = (key) => {
+        const allParameters = this.getParameters();
+        return allParameters[key];
     }
 }
 
@@ -25,3 +40,10 @@ const RouterPath = Object.freeze({
     home: "/",
     landlineService: "/landline",
 });
+
+const RouterPathParameter = Object.freeze({
+    portingActivated: "porting_activated",
+    newNumberActivated: "new_number_activated"
+});
+
+const router = new Router();
