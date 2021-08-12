ca48544b90b93125ab32e9d5a2745817e485d815
Igor M <russo.programmisto@gmail.com>
2021-08-12 23:30:35 +0300

    ....

--- js/pages/onboarding-flow/general/account/index.js
+++ js/pages/onboarding-flow/general/account/index.js
@@ -47,82 +47,81 @@ if (!IS_PRODUCTION) {
             }
         }
     });
-    return;
-}
+} else {
+    $(document).ready(() => {
 
-$(document).ready(() => {
-
-    const formData = {
-        email: ""
-    };
-
-    const handleFormChange = () => {
-        logger.print(`Form has changed`);
-        UserInterface.setElementEnabled(
-            submitButton,
-            new EmailValidator().check(formData.email)
-        );
-    };
-
-    const submitFormData = () => {
-        logger.print(`Submit form data`);
-
-        const email = $(emailTextField).val();
-        
-        if (email.length) {
-            clearOnboardingFlowSettings();
-            Store.local.write(
-                Store.keys.onboardingFlow.email,
-                email
-            );
-            GoogleDocIntegration.addLineToOnboarding(
-                email,
-                undefined,
-                false,
-                undefined,
-                undefined,
-                false,
-                false,
-                (response, error, success) => {
-                    if (isPortingActivated || isNewNumberActivated) {
-                        router.open(
-                            RouterPath.onboarding_onboarding_step_1,
-                            router.getParameters()
-                        );
-                    } else {
-                        router.open(
-                            RouterPath.onboarding_general_account,
-                            router.getParameters()
-                        );
-                    }
-                }
-            );
-        } else {
+        const formData = {
+            email: ""
+        };
+    
+        const handleFormChange = () => {
+            logger.print(`Form has changed`);
             UserInterface.setElementEnabled(
                 submitButton,
-                false
+                new EmailValidator().check(formData.email)
             );
-        }
-    };
+        };
     
-    const submitButton = document.getElementById("submit-button");
+        const submitFormData = () => {
+            logger.print(`Submit form data`);
     
-    const emailTextField = document.getElementById("email");
-    new InputValueObserver(emailTextField).startObserving((newValue) => {
-        formData.email = newValue;
+            const email = $(emailTextField).val();
+            
+            if (email.length) {
+                clearOnboardingFlowSettings();
+                Store.local.write(
+                    Store.keys.onboardingFlow.email,
+                    email
+                );
+                GoogleDocIntegration.addLineToOnboarding(
+                    email,
+                    undefined,
+                    false,
+                    undefined,
+                    undefined,
+                    false,
+                    false,
+                    (response, error, success) => {
+                        if (isPortingActivated || isNewNumberActivated) {
+                            router.open(
+                                RouterPath.onboarding_onboarding_step_1,
+                                router.getParameters()
+                            );
+                        } else {
+                            router.open(
+                                RouterPath.onboarding_general_account,
+                                router.getParameters()
+                            );
+                        }
+                    }
+                );
+            } else {
+                UserInterface.setElementEnabled(
+                    submitButton,
+                    false
+                );
+            }
+        };
+        
+        const submitButton = document.getElementById("submit-button");
+        
+        const emailTextField = document.getElementById("email");
+        new InputValueObserver(emailTextField).startObserving((newValue) => {
+            formData.email = newValue;
+            handleFormChange();
+        });
+    
+        const form = document.getElementById("wf-form-onboarding-flow");
+        $(form).submit((event) => {
+            event.preventDefault();
+            submitFormData();
+        });
+        
+        $(submitButton).on("click", (event) => {
+            event.preventDefault();
+            submitFormData();
+        });
+    
         handleFormChange();
     });
-
-    const form = document.getElementById("wf-form-onboarding-flow");
-    $(form).submit((event) => {
-        event.preventDefault();
-        submitFormData();
-    });
-    
-    $(submitButton).on("click", (event) => {
-        event.preventDefault();
-        submitFormData();
-    });
-
-    handleFormChange();
-});
+}
