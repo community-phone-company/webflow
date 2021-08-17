4502262bfcbb46c8529b59b6d258f1a1140028d2
Igor M <russo.programmisto@gmail.com>
2021-08-17 03:45:30 +0300

    ..........

--- js/pages/checkout-flow/choose-plan/index.js
+++ js/pages/checkout-flow/choose-plan/index.js
@@ -1,3 +1,5 @@
+console.log(`VERSION: `, 1);
+
 /**
  * @param {Product} product Product.
  * @returns {string}
@@ -95,10 +97,8 @@ const getAddonSectionInternalHtmlLayout = (products) => {
     return html;
 };
 
-var choosePlanVM = undefined;
-
 if (router.isTestEnvironment()) {
-    choosePlanVM = new Vue({
+    const choosePlanVM = new Vue({
         data: {
             allProducts: []
         },
@@ -121,111 +121,111 @@ if (router.isTestEnvironment()) {
     ProductStore.getDefault().loadProducts(error => {
         choosePlanVM.allProducts = ProductStore.getDefault().getAllProducts();
     });
-}
+} else {
+    $(document).ready(() => {
 
-$(document).ready(() => {
-
-    const formData = {
-        monthly: $("#monthly-plan").hasClass("w--current"),
-        getNewNumber: $(".tabs_phonenumber_service .tab-new-number").hasClass("w--current"),
-        addHandset: false,
-        addInsurance: false,
-    };
-
-    const getProductIdentifiers = () => {
-        var identifiers = [
-            ProductIdentifier.landlineBase,
-        ];
-
-        if (formData.getNewNumber) {
-            identifiers.push(
-                formData.monthly ? ProductIdentifier.landlinePhoneServiceMonthly : ProductIdentifier.landlinePhoneServiceYearly,
-            );
-        } else {
-            identifiers.push(
-                formData.monthly ? ProductIdentifier.portingLandlineNumberMonthly : ProductIdentifier.portingLandlineNumberYearly
-            );
-        }
-
-        if (formData.addHandset) {
-            identifiers.push(
-                ProductIdentifier.handset
-            );
-        }
-
-        if (formData.addInsurance) {
-            identifiers.push(
-                formData.monthly ? ProductIdentifier.insuranceMonthly : ProductIdentifier.insuranceYearly
-            );
-        }
-
-        return identifiers;
-    };
-
-    $("#tab-new-number").on("click", (event) => {
-        formData.getNewNumber = true;
-    });
-
-    $("#tab-keep-existing-number").on("click", (event) => {
-        formData.getNewNumber = false;
-    });
-
-    $("#monthly-plan").on("click", (event) => {
-        formData.monthly = true;
-    });
-
-    $("#annual-plan").on("click", (event) => {
-        formData.monthly = false;
-    });
-
-    $("#monthly-plan-p").on("click", (event) => {
-        formData.monthly = true;
-    });
-
-    $("#annual-plan-p").on("click", (event) => {
-        formData.monthly = false;
-    });
-
-    $("#handset-addon-card").on("click", (event) => {
-        formData.addHandset = !formData.addHandset;
-    });
-
-    $("#insurance-addon-card").on("click", (event) => {
-        formData.addInsurance = !formData.addInsurance;
-    });
-
-    /**
-     * Here we handle submit button click.
-     */
-    const submitButton = document.querySelectorAll(".continue_choose_plan")[0];
-
-    $(submitButton).on("click", (event) => {
-        const period = (() => {
-            const monthly = $("#monthly-plan").hasClass("w--current");
-            const annual = $("#annual-plan").hasClass("w--current");
-
-            if (monthly) {
-                return "Monthly";
-            } else if (annual) {
-                return "Annual";
+        const formData = {
+            monthly: $("#monthly-plan").hasClass("w--current"),
+            getNewNumber: $(".tabs_phonenumber_service .tab-new-number").hasClass("w--current"),
+            addHandset: false,
+            addInsurance: false,
+        };
+    
+        const getProductIdentifiers = () => {
+            var identifiers = [
+                ProductIdentifier.landlineBase,
+            ];
+    
+            if (formData.getNewNumber) {
+                identifiers.push(
+                    formData.monthly ? ProductIdentifier.landlinePhoneServiceMonthly : ProductIdentifier.landlinePhoneServiceYearly,
+                );
             } else {
-                return "";
+                identifiers.push(
+                    formData.monthly ? ProductIdentifier.portingLandlineNumberMonthly : ProductIdentifier.portingLandlineNumberYearly
+                );
             }
-        })();
-
-        Store.local.write(Store.keys.checkoutFlow.getNewNumber, formData.getNewNumber);
-        Store.local.write(Store.keys.checkoutFlow.period, period);
-        Store.local.write(Store.keys.checkoutFlow.addHandsetPhone, formData.addHandset);
-        Store.local.write(Store.keys.checkoutFlow.addInsurance, formData.addInsurance);
-        Store.local.write(Store.keys.checkoutFlow.selectedProductIdentifiers, getProductIdentifiers());
+    
+            if (formData.addHandset) {
+                identifiers.push(
+                    ProductIdentifier.handset
+                );
+            }
+    
+            if (formData.addInsurance) {
+                identifiers.push(
+                    formData.monthly ? ProductIdentifier.insuranceMonthly : ProductIdentifier.insuranceYearly
+                );
+            }
+    
+            return identifiers;
+        };
+    
+        $("#tab-new-number").on("click", (event) => {
+            formData.getNewNumber = true;
+        });
+    
+        $("#tab-keep-existing-number").on("click", (event) => {
+            formData.getNewNumber = false;
+        });
+    
+        $("#monthly-plan").on("click", (event) => {
+            formData.monthly = true;
+        });
+    
+        $("#annual-plan").on("click", (event) => {
+            formData.monthly = false;
+        });
+    
+        $("#monthly-plan-p").on("click", (event) => {
+            formData.monthly = true;
+        });
+    
+        $("#annual-plan-p").on("click", (event) => {
+            formData.monthly = false;
+        });
+    
+        $("#handset-addon-card").on("click", (event) => {
+            formData.addHandset = !formData.addHandset;
+        });
+    
+        $("#insurance-addon-card").on("click", (event) => {
+            formData.addInsurance = !formData.addInsurance;
+        });
+    
+        /**
+         * Here we handle submit button click.
+         */
+        const submitButton = document.querySelectorAll(".continue_choose_plan")[0];
+    
+        $(submitButton).on("click", (event) => {
+            const period = (() => {
+                const monthly = $("#monthly-plan").hasClass("w--current");
+                const annual = $("#annual-plan").hasClass("w--current");
+    
+                if (monthly) {
+                    return "Monthly";
+                } else if (annual) {
+                    return "Annual";
+                } else {
+                    return "";
+                }
+            })();
+    
+            Store.local.write(Store.keys.checkoutFlow.getNewNumber, formData.getNewNumber);
+            Store.local.write(Store.keys.checkoutFlow.period, period);
+            Store.local.write(Store.keys.checkoutFlow.addHandsetPhone, formData.addHandset);
+            Store.local.write(Store.keys.checkoutFlow.addInsurance, formData.addInsurance);
+            Store.local.write(Store.keys.checkoutFlow.selectedProductIdentifiers, getProductIdentifiers());
+        });
+    
+        /**
+         * Send user's data to Active Campaign.
+         */
+        exportCheckoutFlowDataToActiveCampaign(
+            (response, error, success) => {
+                console.log("Active Campaign");
+            }
+        );
     });
-
-    /**
-     * Send user's data to Active Campaign.
-     */
-    exportCheckoutFlowDataToActiveCampaign(
-        (response, error, success) => {
-            console.log("Active Campaign");
-        }
-    );
-});
+}
