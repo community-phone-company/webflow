367d3660af217f084ef9d41cd722d80f8f91433f
Igor M <russo.programmisto@gmail.com>
2021-08-20 05:51:40 +0300

    .....

--- js/pages/checkout-flow/order-summary-panel.js
+++ js/pages/checkout-flow/order-summary-panel.js
@@ -5,19 +5,19 @@ class OrderSummaryPanel {
      */
     constructor(container) {
         if (typeof container === "string") {
-            this.container = document.querySelectorAll(container)[0];
+            this.containerDiv = document.querySelectorAll(container)[0];
         } else if (container instanceof HTMLDivElement) {
-            this.container = this.container;
+            this.containerDiv = this.containerDiv;
         } else {
             throw new Error("Container not found");
         }
 
         this.cards = {
             dueToday: new OrderSummaryPanelCard(
-                this.container.querySelectorAll("#due-today-card")[0]
+                this.containerDiv.querySelectorAll("#due-today-card")[0]
             ),
             service: new OrderSummaryPanelCard(
-                this.container.querySelectorAll("#in-15-days-card")[0]
+                this.containerDiv.querySelectorAll("#in-15-days-card")[0]
             )
         };
     }
