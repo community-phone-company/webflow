7cadbd1fddbde25b47768f354bb611dbf8006683
Igor M <russo.programmisto@gmail.com>
2021-08-25 19:40:32 +0300

    ..........

--- js/shared/math.js
+++ js/shared/math.js
@@ -1,3 +1,19 @@
+/**
+ * @param {number} number 
+ * @param {number} precision 
+ * @returns {number}
+ */
 Math.roundUp = function(number, precision) { 
     return Math.ceil(number * Math.pow(10, precision)) / Math.pow(10, precision);
+}
+
+/**
+ * @param {number} amount
+ * @param {boolean} roundUp Defines whether to round up the amount.
+ * @returns {string}
+ */
+Math.formatPrice = function(amount, roundUp) {
+    var value = roundUp ? this.roundUp(amount) : amount;
+    const hasFractionalPart = (value % 1) > 0;
+    return hasFractionalPart ? value.toFixed(2) : value;
 }
