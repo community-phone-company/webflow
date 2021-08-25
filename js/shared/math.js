059e51f0a5f91aa75e43f7e9fdd4b71ec0a3d2ca
Igor M <russo.programmisto@gmail.com>
2021-08-25 19:43:53 +0300

    ....

--- js/shared/math.js
+++ js/shared/math.js
@@ -13,7 +13,7 @@ Math.roundUp = function(number, precision) {
  * @returns {string}
  */
 Math.formatPrice = function(amount, roundUp) {
-    var value = roundUp ? this.roundUp(amount) : amount;
+    var value = roundUp ? this.roundUp(amount, 2) : amount;
     const hasFractionalPart = (value % 1) > 0;
     return hasFractionalPart ? value.toFixed(2) : value;
 }
