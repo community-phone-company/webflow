182044e4f0a1894704e5197e12ccc41b30d51f0c
Igor M <russo.programmisto@gmail.com>
2021-08-31 22:09:09 +0300

    ....

--- js/shared/popup.js
+++ js/shared/popup.js
@@ -56,6 +56,14 @@ class Popup {
     }
 
     /**
+     * Container.
+     * @returns `HTMLElement` instance.
+     */
+    getContainer = () => {
+        return this._container;
+    }
+
+    /**
      * @returns {string}
      */
     getTitle = () => {
--- js/shared/user-portal-manager.js
+++ js/shared/user-portal-manager.js
@@ -134,5 +134,47 @@ class UserPortalManager {
             popup.show();
             console.log("Open user portal popup");
         });
+
+        const elements = {
+            container: popup.getContainer(),
+            steps: {
+                one: {
+                    container: popup.getContainer().querySelectorAll(".sign-up-step-1")[0],
+                    emailInput: popup.getContainer().querySelectorAll(".sign-up-step-1 input.email-input")[0],
+                    ctaButton: popup.getContainer().querySelectorAll(".sign-up-step-1 .popup-cta-button")[0]
+                },
+                two: {
+                    container: popup.getContainer().querySelectorAll(".sign-up-step-2")[0],
+                    ctaButton: popup.getContainer().querySelectorAll(".sign-up-step-2 .popup-cta-button")[0]
+                }
+            }
+        };
+
+        const PopupState = Object.freeze({
+            inputEmail: "input-email",
+            inputCode: "input-code"
+        });
+        const setState = (state) => {
+            $([elements.steps.one.container, elements.steps.two.container]).hide();
+            const containerToDisplay = (() => {
+                switch (state) {
+                    case PopupState.inputEmail: {
+                        return elements.steps.one.container;
+                    }
+                    case PopupState.inputCode: {
+                        return elements.steps.two.container;
+                    }
+                }
+            })();
+            $(containerToDisplay).show();
+        };
+
+        $(elements.steps.one.ctaButton).on("click", (event) => {
+            event.preventDefault();
+        });
+
+        $(elements.steps.two.ctaButton).on("click", (event) => {
+            event.preventDefault();
+        });
     }
 }
