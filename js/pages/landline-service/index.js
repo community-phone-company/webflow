f45490bef59f8f6427a68e38921bfa27392fbc73
Igor M <russo.programmisto@gmail.com>
2021-09-30 14:48:41 +0300

    ...

--- js/pages/landline-service/index.js
+++ js/pages/landline-service/index.js
@@ -340,7 +340,7 @@ $(document).ready(() => {
     checkCoverageVM.handleDataChange();
 
     setTimeout(() => {
-        $("#check-coverage-middle").off().on("click", (e) => {
+        $("#check-coverage-middle").off().on("click change touchstart tap", (e) => {
             e.preventDefault();
             alert("Test");
         });
@@ -348,33 +348,3 @@ $(document).ready(() => {
         init();
     }, 4000);
 });
-
-function touchHandler(event){
-    var touches = event.changedTouches,
-        first = touches[0],
-        type = "";
-
-    switch(event.type)
-    {
-       case "touchstart": type = "mousedown"; break;
-       case "touchmove":  type = "mousemove"; break;        
-       case "touchend":   type = "mouseup"; break;
-       default: return;
-    }
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
-                          first.screenX, first.screenY, 
-                          first.clientX, first.clientY, false, 
-                          false, false, false, 0/*left*/, null);
-
-    first.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-
-function init() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);    
-}
