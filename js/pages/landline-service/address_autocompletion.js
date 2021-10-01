9bb0fb945210a1a7d0b06e3f2b2956860bc8c92a
Igor M <russo.programmisto@gmail.com>
2021-10-01 20:25:31 +0300

    ....

---
+++ js/pages/landline-service/address_autocompletion.js
@@ -0,0 +1,33 @@
+const addressAutocompletion = {
+    container: document.querySelectorAll("#service-address-autocomplete-suggestions-container")[0]
+};
+
+/**
+ * @param {string} address 
+ * @param {string} highlightedSubstring 
+ * @returns {string}
+ */
+const getHtmlForAddressAutocompletionItem = (address, highlightedSubstring) => {
+    return `
+        <div class="autocomplete-item">
+            <div class="highlited-autocomplete-label">
+                ${address}
+            </div>
+        </div>
+    `.replaceAll(
+        highlightedSubstring,
+        `<span class="autocomplete-label">${highlightedSubstring}</span>`
+    );
+}
+
+/**
+ * @param {string[]} addresses 
+ * @param {string} hightlightedSubstring 
+ */
+const setAutocompletionItems = (addresses, hightlightedSubstring) => {
+    const html = addresses.reduce(
+        (previous, current) => `${previous}${current}`,
+        ""
+    );
+    $(addressAutocompletion).html(html);
+};
