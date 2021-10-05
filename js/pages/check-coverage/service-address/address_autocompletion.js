aa7fc288996092c337a6bdccddf9a80b0f53cf70
Igor M <russo.programmisto@gmail.com>
2021-10-06 00:44:24 +0300

    ....

---
+++ js/pages/check-coverage/service-address/address_autocompletion.js
@@ -0,0 +1,86 @@
+/**
+ * @returns {HTMLElement}
+ */
+const getAddressAutocompletionContainer = () => {
+    return document.querySelectorAll("#service-address-autocomplete-suggestions-container")[0];
+};
+
+/**
+ * @param {AddressSuggestion} addressSuggestion 
+ * @param {string} highlightedSubstring 
+ * @returns {string}
+ */
+const getHtmlForAddressAutocompletionItem = (addressSuggestion, highlightedSubstring) => {
+    const address = `${addressSuggestion.primaryLine}, ${addressSuggestion.city}, ${addressSuggestion.state} ${addressSuggestion.zipCode}`;
+    return `
+        <div class="autocomplete-item" address-suggestion="${addressSuggestion.serialize()}">
+            <div class="highlited-autocomplete-label">
+                ${address.replaceAll(
+        highlightedSubstring,
+        `<span class="autocomplete-label">${highlightedSubstring}</span>`
+    )}
+            </div>
+        </div>
+    `;
+}
+
+/**
+ * @param {HTMLElement} element 
+ * @returns {AddressSuggestion}
+ */
+const getAddressSuggestionFromAutocompletionItem = (element) => {
+    const serialized = $(element).attr("address-suggestion");
+    return AddressSuggestion.deserialize(
+        serialized
+    );
+};
+
+/**
+ * @param {string[]} addresses 
+ * @param {string} highlightedSubstring 
+ * @param {(suggestion: AddressSuggestion) => void} onSuggestionSelected
+ */
+const setAutocompletionItems = (addresses, highlightedSubstring, onSuggestionSelected) => {
+    const container = getAddressAutocompletionContainer();
+
+    if (addresses.length) {
+        const html = addresses
+            .map(address => {
+                return getHtmlForAddressAutocompletionItem(
+                    address,
+                    highlightedSubstring
+                );
+            })
+            .reduce(
+                (previous, current) => `${previous}${current}`,
+                ""
+            );
+        $(container).html(html);
+        showAutocompletionItems();
+
+        $(".autocomplete-item").off().on("click", (event) => {
+            event.preventDefault();
+            const selectedItem = event.currentTarget;
+            const selectedAddressSuggestion = getAddressSuggestionFromAutocompletionItem(
+                selectedItem
+            );
+
+            if (onSuggestionSelected) {
+                onSuggestionSelected(
+                    selectedAddressSuggestion
+                );
+            }
+        });
+    } else {
+        hideAutocompletionItems();
+        $(container).html(``);
+    }
+};
+
+const showAutocompletionItems = () => {
+    $(getAddressAutocompletionContainer()).show();
+};
+
+const hideAutocompletionItems = () => {
+    $(getAddressAutocompletionContainer()).hide();
+};
---
+++ js/pages/check-coverage/service-address/index.js
@@ -0,0 +1,119 @@
+const page = {
+    elements: {
+        form: document.querySelectorAll("#wf-form-service-address")[0],
+        addressLineOneInput: document.querySelectorAll("#service-address-line-one-input")[0],
+        addressLineTwoInput: document.querySelectorAll("#service-address-line-two-input")[0],
+        cityInput: document.querySelectorAll("#service-address-city-input")[0],
+        zipInput: document.querySelectorAll("#service-address-zip-input")[0],
+        stateSelect: document.querySelectorAll("#service-address-state-input")[0],
+        submitButton: document.querySelectorAll("#submit-button")[0]
+    },
+    data: {
+        addressLineOne: "",
+        addressLineTwo: "",
+        city: "",
+        zip: "",
+        state: "AL",
+        isBusiness: false,
+        lastSelectedAddressSuggestion: undefined
+    }
+};
+
+/**
+ * @param {string} searchQuery 
+ */
+ const onRequestedAddressSuggestions = (searchQuery) => {
+    if (!IS_PRODUCTION) {
+        const isSearchQueryEmpty = !searchQuery.length;
+        const isSearchQueryEqualToLastSelectedSuggestion = searchQuery === (page.data.lastSelectedAddressSuggestion && page.data.lastSelectedAddressSuggestion.primaryLine);
+
+        if (isSearchQueryEmpty || isSearchQueryEqualToLastSelectedSuggestion) {
+            setAutocompletionItems([]);
+        } else {
+            addressSuggestionsManager.getAutocompletions(searchQuery, (results, error) => {
+                setAutocompletionItems(results, searchQuery, suggestion => {
+                    setAutocompletionItems([]);
+                    page.data.lastSelectedAddressSuggestion = suggestion;
+                    page.elements.addressLineOneInput.value = suggestion.primaryLine;
+                    page.elements.addressLineTwoInput.value = "";
+                    page.elements.cityInput.value = suggestion.city;
+                    page.elements.zipInput.value = suggestion.zipCode;
+                    page.elements.stateSelect.value = suggestion.state;
+                });
+            });
+        }
+    }
+};
+new InputValueObserver(page.elements.addressLineOneInput).startObserving(newValue => {
+    page.data.addressLineOne = newValue;
+    handleDataChange();
+    onRequestedAddressSuggestions(
+        newValue
+    );
+});
+page.elements.addressLineOneInput.onfocus = () => {
+    onRequestedAddressSuggestions(
+        page.elements.addressLineOneInput.value
+    );
+};
+page.elements.addressLineOneInput.onfocusout = () => {
+    // setAutocompletionItems([]);
+};
+new InputValueObserver(page.elements.addressLineTwoInput).startObserving(newValue => {
+    page.data.addressLineTwo = newValue;
+    handleDataChange();
+});
+new InputValueObserver(page.elements.cityInput).startObserving(newValue => {
+    page.data.city = newValue;
+    handleDataChange();
+});
+new InputValueObserver(page.elements.zipInput).startObserving(newValue => {
+    page.data.zip = newValue;
+    handleDataChange();
+});
+new InputValueObserver(page.elements.stateSelect).startObserving(newValue => {
+    page.data.state = newValue;
+    handleDataChange();
+});
+
+const handleDataChange = () => {
+    const isFormValid = page.data.addressLineOne.length > 0
+        && page.data.city.length > 0
+        && page.data.zip.length == 5
+        && page.data.state.length > 0;
+    console.log(`is form valid: ${isFormValid}`);
+    UserInterface.setElementEnabled(
+        page.elements.submitButton(),
+        isFormValid
+    );
+};
+
+/**
+ * @param {(isCorrect: boolean) => void} callback 
+ */
+const isAddressCorrect = (callback) => {
+    const billingAddress = new ProductCartBillingAddress(
+        page.data.city,
+        page.data.state,
+        page.data.zip
+    );
+
+    const productCart = new ProductCart();
+    productCart.setBillingAddress(
+        billingAddress    
+    );
+    productCart.addProductIdentifier("landline-phone-service-monthly");
+    productCart.addProductIdentifier("shipmonk-box-without-handset");
+    productCart.updatePrices((error) => {
+        const isCorrect = error == undefined;
+        callback(
+            isCorrect
+        );
+    });
+};
+
+$(page.elements.form).submit(event => {
+    event.preventDefault();
+    console.log("SUBMIT");
+    return false;
+});
