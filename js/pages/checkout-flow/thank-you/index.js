a9b6c0beddca7fd597dffecb0e3e2d9ed9ce08f8
Igor M <russo.programmisto@gmail.com>
2021-10-08 15:32:02 +0300

    .............

--- js/pages/checkout-flow/thank-you/index.js
+++ js/pages/checkout-flow/thank-you/index.js
@@ -1,11 +1,37 @@
+const page = {
+    elements: {
+        selectedPhoneNumberLink: document.getElementById("selected-phone-number-link"),
+        selectedPhoneNumberText: document.getElementById("selected-phone-number-text")
+    },
+    data: {
+        selectedPhoneNumber: (() => {
+            const serialized = Store.local.read(
+                Store.keys.checkoutFlow.selectedPhoneNumber
+            );
+            return typeof serialized === "string"
+                ? PhoneNumber.deserialize(serialized)
+                : undefined;
+        })()
+    }
+};
+
+const setupUI = () => {
+    if (IS_PRODUCTION) {
+        return;
+    }
+
+    if (page.data.selectedPhoneNumber) {
+        $(page.elements.selectedPhoneNumberLink).attr(
+            "href",
+            `+1${page.data.selectedPhoneNumber.formatted(PhoneNumberFormatStyle.regular)}`
+        );
+        $(page.elements.selectedPhoneNumberText).html(
+            page.data.selectedPhoneNumber.formatted(PhoneNumberFormatStyle.brackets)
+        );
+        $(page.elements.selectedPhoneNumberLink).show();
+    }
+};
+
 $(document).ready(() => {
-    
-    /**
-     * Send user's data to Active Campaign.
-     */
-    exportCheckoutFlowDataToActiveCampaign(
-        (response, error, success) => {
-            console.log("Active Campaign");
-        }
-    );
+    setupUI();
 });
