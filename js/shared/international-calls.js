0f9ea3e86e0901dff7ce038d0ad8551bf4b92cbc
Igor Matyushkin <russo.programmisto@gmail.com>
2021-11-03 23:00:47 +0300

    ....

--- js/shared/international-calls.js
+++ js/shared/international-calls.js
@@ -14,7 +14,9 @@ class CountryCallRate {
     getCountryFlagUrl() {
         const baseUrl = "https://community-phone-bucket.nyc3.digitaloceanspaces.com/webflow/img/country-flags";
         const imageName = (() => {
-            if (this.countryCode === "N/A") {
+            const countriesWithDefaultFlag = ["N/A", "ASC", "CK", "IO", "FO", "GF", "PYF", "GL", "GP", "KI", "KNA", "XK", "MF", "MH", "YT", "NR", "NC", "NU", "NF", "PS", "PM", "RE", "SS", "TV", "WF"];
+
+            if (countriesWithDefaultFlag.includes(this.countryCode)) {
                 return `WW.svg`;
             } else {
                 return `${this.countryCode.toUpperCase()}.svg`;
