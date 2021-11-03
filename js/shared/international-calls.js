025a9f467e82f725af04d026d6a67a230a6bf9ff
Igor Matyushkin <russo.programmisto@gmail.com>
2021-11-03 19:14:34 +0300

    ....

---
+++ js/shared/international-calls.js
@@ -0,0 +1,35 @@
+class CountryCallRate {
+
+    /**
+     * @param {any} json 
+     */
+    constructor(json) {
+        this.countryName = json.name;
+        this.countryCode = json.country_code;
+        this.phoneCode = json.phone_code;
+        this.minRate = json.rates.min;
+        this.maxRate = json.rates.max;
+    }
+}
+
+/**
+ * @param {(rates: CountryCallRate[], error: any) => void} callback 
+ * @returns {XMLHttpRequest}
+ */
+const getInternationalCallRatesForAllCountries = (callback) => {
+    const api = CommunityPhoneAPI.currentEnvironmentWithVersion("2");
+    return api.jsonRequest(
+        "",
+        "GET",
+        undefined,
+        undefined,
+        (response, error) => {
+            if (error) {
+                callback([], error);
+            } else {
+                const rates = response.countries.map(el => new CountryCallRate(el));
+                callback(rates, undefined);
+            }
+        }
+    );
+};
