ddc74ee090557c8cd75a8bb2566bcd4cc067d033
Igor Matyushkin <russo.programmisto@gmail.com>
2021-11-16 01:30:19 +0300

    .....

---
+++ js/shared/paging.js
@@ -0,0 +1,11 @@
+class Paging {
+
+    /**
+     * @param {number} pageIndex 
+     * @param {number} itemsPerPage 
+     */
+    constructor(pageIndex, itemsPerPage) {
+        this.pageIndex = pageIndex;
+        this.itemsPerPage = itemsPerPage;
+    }
+}
