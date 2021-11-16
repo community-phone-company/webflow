d3ef0c795b66ea6876ca0872749a67a162f1bac5
Igor Matyushkin <russo.programmisto@gmail.com>
2021-11-16 16:14:32 +0300

    ....

---
+++ js/shared/state-list.js
@@ -0,0 +1,627 @@
+const RESULT = [
+    {
+        stateName: "Alabama",
+        stateCode: "AL",
+        areaCodes: [
+            "205",
+            "251",
+            "256",
+            "334",
+            "938"
+        ]
+    },
+    {
+        stateName: "Alaska",
+        stateCode: "AK",
+        areaCodes: [
+            "907"
+        ]
+    },
+    {
+        stateName: "Arizona",
+        stateCode: "AZ",
+        areaCodes: [
+            "480",
+            "520",
+            "602",
+            "623",
+            "928"
+        ]
+    },
+    {
+        stateName: "Arkansas",
+        stateCode: "AR",
+        areaCodes: [
+            "479",
+            "501",
+            "870"
+        ]
+    },
+    {
+        stateName: "California",
+        stateCode: "CA",
+        areaCodes: [
+            "209",
+            "213",
+            "279",
+            "310",
+            "323",
+            "408",
+            "415",
+            "424",
+            "442",
+            "510",
+            "530",
+            "559",
+            "562",
+            "619",
+            "626",
+            "628",
+            "650",
+            "657",
+            "661",
+            "669",
+            "707",
+            "714",
+            "747",
+            "760",
+            "805",
+            "818",
+            "820",
+            "831",
+            "858",
+            "909",
+            "916",
+            "925",
+            "949",
+            "951"
+        ]
+    },
+    {
+        stateName: "Colorado",
+        stateCode: "CO",
+        areaCodes: [
+            "303",
+            "719",
+            "720",
+            "970"
+        ]
+    },
+    {
+        stateName: "Connecticut",
+        stateCode: "CT",
+        areaCodes: [
+            "203",
+            "475",
+            "860",
+            "959"
+        ]
+    },
+    {
+        stateName: "Delaware",
+        stateCode: "DE",
+        areaCodes: [
+            "302"
+        ]
+    },
+    {
+        stateName: "Florida",
+        stateCode: "FL",
+        areaCodes: [
+            "239",
+            "305",
+            "321",
+            "352",
+            "386",
+            "407",
+            "561",
+            "727",
+            "754",
+            "772",
+            "786",
+            "813",
+            "850",
+            "863",
+            "904",
+            "941",
+            "954"
+        ]
+    },
+    {
+        stateName: "Georgia",
+        stateCode: "GA",
+        areaCodes: [
+            "229",
+            "404",
+            "470",
+            "478",
+            "678",
+            "706",
+            "762",
+            "770",
+            "912"
+        ]
+    },
+    {
+        stateName: "Hawaii",
+        stateCode: "HI",
+        areaCodes: [
+            "808"
+        ]
+    },
+    {
+        stateName: "Idaho",
+        stateCode: "ID",
+        areaCodes: [
+            "208",
+            "986"
+        ]
+    },
+    {
+        stateName: "Illinois",
+        stateCode: "IL",
+        areaCodes: [
+            "217",
+            "224",
+            "309",
+            "312",
+            "331",
+            "618",
+            "630",
+            "708",
+            "773",
+            "779",
+            "815",
+            "847",
+            "872"
+        ]
+    },
+    {
+        stateName: "Indiana",
+        stateCode: "IN",
+        areaCodes: [
+            "219",
+            "260",
+            "317",
+            "463",
+            "574",
+            "765",
+            "812",
+            "930"
+        ]
+    },
+    {
+        stateName: "Iowa",
+        stateCode: "IA",
+        areaCodes: [
+            "319",
+            "515",
+            "563",
+            "641",
+            "712"
+        ]
+    },
+    {
+        stateName: "Kansas",
+        stateCode: "KS",
+        areaCodes: [
+            "316",
+            "620",
+            "785",
+            "913"
+        ]
+    },
+    {
+        stateName: "Kentucky",
+        stateCode: "KY",
+        areaCodes: [
+            "270",
+            "364",
+            "502",
+            "606",
+            "859"
+        ]
+    },
+    {
+        stateName: "Louisiana",
+        stateCode: "LA",
+        areaCodes: [
+            "225",
+            "318",
+            "337",
+            "504",
+            "985"
+        ]
+    },
+    {
+        stateName: "Maine",
+        stateCode: "ME",
+        areaCodes: [
+            "207"
+        ]
+    },
+    {
+        stateName: "Maryland",
+        stateCode: "MD",
+        areaCodes: [
+            "240",
+            "301",
+            "410",
+            "443",
+            "667"
+        ]
+    },
+    {
+        stateName: "Massachusetts",
+        stateCode: "MA",
+        areaCodes: [
+            "339",
+            "351",
+            "413",
+            "508",
+            "617",
+            "774",
+            "781",
+            "857",
+            "978"
+        ]
+    },
+    {
+        stateName: "Michigan",
+        stateCode: "MI",
+        areaCodes: [
+            "231",
+            "248",
+            "269",
+            "313",
+            "517",
+            "586",
+            "616",
+            "734",
+            "810",
+            "906",
+            "947",
+            "989"
+        ]
+    },
+    {
+        stateName: "Minnesota",
+        stateCode: "MN",
+        areaCodes: [
+            "218",
+            "320",
+            "507",
+            "612",
+            "651",
+            "763",
+            "952"
+        ]
+    },
+    {
+        stateName: "Mississippi",
+        stateCode: "MS",
+        areaCodes: [
+            "228",
+            "601",
+            "662",
+            "769"
+        ]
+    },
+    {
+        stateName: "Missouri",
+        stateCode: "MO",
+        areaCodes: [
+            "314",
+            "417",
+            "573",
+            "636",
+            "660",
+            "816"
+        ]
+    },
+    {
+        stateName: "Montana",
+        stateCode: "MT",
+        areaCodes: [
+            "406"
+        ]
+    },
+    {
+        stateName: "Nebraska",
+        stateCode: "NE",
+        areaCodes: [
+            "308",
+            "402",
+            "531"
+        ]
+    },
+    {
+        stateName: "Nevada",
+        stateCode: "NV",
+        areaCodes: [
+            "702",
+            "725",
+            "775"
+        ]
+    },
+    {
+        stateName: "New Hampshire",
+        stateCode: "NH",
+        areaCodes: [
+            "603"
+        ]
+    },
+    {
+        stateName: "New Jersey",
+        stateCode: "NJ",
+        areaCodes: [
+            "201",
+            "551",
+            "609",
+            "640",
+            "732",
+            "848",
+            "856",
+            "862",
+            "908",
+            "973"
+        ]
+    },
+    {
+        stateName: "New Mexico",
+        stateCode: "NM",
+        areaCodes: [
+            "505",
+            "575"
+        ]
+    },
+    {
+        stateName: "New York",
+        stateCode: "NY",
+        areaCodes: [
+            "212",
+            "315",
+            "332",
+            "347",
+            "516",
+            "518",
+            "585",
+            "607",
+            "631",
+            "646",
+            "680",
+            "716",
+            "718",
+            "838",
+            "845",
+            "914",
+            "917",
+            "929",
+            "934"
+        ]
+    },
+    {
+        stateName: "North Carolina",
+        stateCode: "NC",
+        areaCodes: [
+            "252",
+            "336",
+            "704",
+            "743",
+            "828",
+            "910",
+            "919",
+            "980",
+            "984"
+        ]
+    },
+    {
+        stateName: "North Dakota",
+        stateCode: "ND",
+        areaCodes: [
+            "701"
+        ]
+    },
+    {
+        stateName: "Ohio",
+        stateCode: "OH",
+        areaCodes: [
+            "216",
+            "220",
+            "234",
+            "330",
+            "380",
+            "419",
+            "440",
+            "513",
+            "567",
+            "614",
+            "740",
+            "937"
+        ]
+    },
+    {
+        stateName: "Oklahoma",
+        stateCode: "OK",
+        areaCodes: [
+            "405",
+            "539",
+            "580",
+            "918"
+        ]
+    },
+    {
+        stateName: "Oregon",
+        stateCode: "OR",
+        areaCodes: [
+            "458",
+            "503",
+            "541",
+            "971"
+        ]
+    },
+    {
+        stateName: "Pennsylvania",
+        stateCode: "PA",
+        areaCodes: [
+            "215",
+            "223",
+            "267",
+            "272",
+            "412",
+            "445",
+            "484",
+            "570",
+            "610",
+            "717",
+            "724",
+            "814",
+            "878"
+        ]
+    },
+    {
+        stateName: "Rhode Island",
+        stateCode: "RI",
+        areaCodes: [
+            "401"
+        ]
+    },
+    {
+        stateName: "South Carolina",
+        stateCode: "SC",
+        areaCodes: [
+            "803",
+            "843",
+            "854",
+            "864"
+        ]
+    },
+    {
+        stateName: "South Dakota",
+        stateCode: "SD",
+        areaCodes: [
+            "605"
+        ]
+    },
+    {
+        stateName: "Tennessee",
+        stateCode: "TN",
+        areaCodes: [
+            "423",
+            "615",
+            "629",
+            "731",
+            "865",
+            "901",
+            "931"
+        ]
+    },
+    {
+        stateName: "Texas",
+        stateCode: "TX",
+        areaCodes: [
+            "210",
+            "214",
+            "254",
+            "281",
+            "325",
+            "346",
+            "361",
+            "409",
+            "430",
+            "432",
+            "469",
+            "512",
+            "682",
+            "713",
+            "726",
+            "737",
+            "806",
+            "817",
+            "830",
+            "832",
+            "903",
+            "915",
+            "936",
+            "940",
+            "956",
+            "972",
+            "979"
+        ]
+    },
+    {
+        stateName: "Utah",
+        stateCode: "UT",
+        areaCodes: [
+            "385",
+            "435",
+            "801"
+        ]
+    },
+    {
+        stateName: "Vermont",
+        stateCode: "VT",
+        areaCodes: [
+            "802"
+        ]
+    },
+    {
+        stateName: "Virginia",
+        stateCode: "VA",
+        areaCodes: [
+            "276",
+            "434",
+            "540",
+            "571",
+            "703",
+            "757",
+            "804"
+        ]
+    },
+    {
+        stateName: "Washington",
+        stateCode: "WA",
+        areaCodes: [
+            "206",
+            "253",
+            "360",
+            "425",
+            "509",
+            "564"
+        ]
+    },
+    {
+        stateName: "Washington, DC",
+        stateCode: "DC",
+        areaCodes: [
+            "202"
+        ]
+    },
+    {
+        stateName: "West Virginia",
+        stateCode: "WV",
+        areaCodes: [
+            "304",
+            "681"
+        ]
+    },
+    {
+        stateName: "Wisconsin",
+        stateCode: "WI",
+        areaCodes: [
+            "262",
+            "414",
+            "534",
+            "608",
+            "715",
+            "920"
+        ]
+    },
+    {
+        stateName: "Wyoming",
+        stateCode: "WY",
+        areaCodes: [
+            "307"
+        ]
+    }
+];
