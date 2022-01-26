ba7c20f884c38b1755e6a31e19b06a5f1dccf57c
Max Kolomiyets <therealwacs@gmail.com>
2022-01-26 18:57:53 +0200

    Updated schedule.js

--- js/shared/schedule.js
+++ js/shared/schedule.js
@@ -1,8 +1,15 @@
-function isWeekend() {
+/*
+* Dynamically change hours based on day
+*/
+
+const isWeekend = (() => {
   const today = new Date().getDay();
   return (today == 6 || today == 0);
+})();
+
+const schedulePlace = $("#header-schedule"),
+      schedule = isWeekend ? '10am - 7pm ET Sat/Sun' : '7am - 10pm Mon-Fri';
+
+if (schedulePlace) {
+  $(schedulePlace).text(schedule);
 }
-
-const schedule = (isWeekend()) ? '10am - 7pm ET Sat/Sun' : '7am - 10pm Mon-Fri';
-
-$('#header-schedule').text(schedule);
