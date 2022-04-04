eb71b7450361f3063e0d411b09064a71711ae313
Igor M <russo.programmisto@gmail.com>
2022-04-04 19:06:15 +0300

    ...

---
+++ js/shared/date-formatter.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number} day 
+ * @param {number} month 
+ * @param {boolean} shortened 
+ * @returns 
+ */
+export const getStringFromDayAndMonth = (
+    day,
+    month,
+    shortened
+) => {
+    const formatter = new Intl.DateTimeFormat(
+        "en",
+        {
+            month: shortened ? "short" : "long",
+            day: "2-digit"
+        }
+    );
+    return formatter.format(
+        new Date(0, month - 1, day)
+    );
+}
+
+/**
+ * @param {number} timestamp 
+ * @param {number} timezone 
+ * @param {string} locale 
+ * @returns {string}
+ */
+export const getFormattedDateAndTime = (
+    timestamp,
+    timezone,
+    locale
+) => {
+    var date = new Date(timestamp);
+    date = new Date(timestamp + (date.getTimezoneOffset() + timezone * 60) * 60000);
+    return date.toLocaleString(locale);
+}
+
+/**
+ * @param {number} timestamp 
+ * @returns {string}
+ */
+const getFormattedDateAndTimeForBoston = (timestamp) => {
+    return getFormattedDateAndTime(
+        timestamp,
+        -5,
+        "en-US"
+    );
+}
