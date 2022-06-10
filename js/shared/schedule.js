282fc2aba88ff5e65531c0d377a00f4e9d2c0256
Igor Matyushkin <russo.programmisto@gmail.com>
2022-06-10 11:01:38 +0300

    ...

--- js/shared/schedule.js
+++ js/shared/schedule.js
@@ -7,5 +7,5 @@ const isWeekend = (() => {
     return (today == 6 || today == 0);
 })();
 
-const schedule = isWeekend ? '10am - 7pm ET Sat/Sun' : '7am - 10pm Mon-Fri';
+const schedule = isWeekend ? '10am - 7pm ET Sat/Sun' : '10am - 7pm Mon-Fri';
 $("#header-schedule").text(schedule);
